69e50f7532fcc35ab2beb1077626434ccbe78ead
Vedant0370 <vedant.0370@gmail.com>
2023-12-13 17:25:00 +0530

    update Enquiry.js

--- Backend/src/routes/Enquiry.js
+++ Backend/src/routes/Enquiry.js
@@ -36,34 +36,41 @@ router.get("/", async (req, res) => {
 });
 
 // POST route - Create a new enquiry
-router.post("/", async (req, res) => {
-  const enquiry = new newEnquiry(req.body);
-
-  try {
-    const newEnquiryRecord = await enquiry.save();
-    res.status(201).json(newEnquiryRecord);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
-
-
 // POST route - Create a new enquiry
 router.post("/", async (req, res) => {
   const enquiry = new newEnquiry(req.body);
 
   try {
     const newEnquiryRecord = await enquiry.save();
-    const responseData = {
-      _id: newEnquiryRecord._id,
-      filename: newEnquiryRecord.filename,
-      path: newEnquiryRecord.path,
-      serviceImage: newEnquiryRecord.serviceImage,
-      __v: newEnquiryRecord.__v,
-    };
+    const responseData = await newEnquiry.findById(newEnquiryRecord._id);
     res.status(201).json(responseData);
   } catch (error) {
     res.status(400).json({ message: error.message });
+  }
+});
+
+
+
+// POST route - Create a new enquiry
+router.post('/', upload.single('image'), async (req, res) => {
+  try {
+    if (req.file) {
+      const publicUrl = `https://executive-grapeseed.onrender.com/public/uploads/${req.file.originalname}`;
+
+      const imageData = new newEnquiry({
+        filename: req.file.originalname,
+        path: req.file.path, // This is the local path on your server
+        serviceImage: publicUrl,
+      });
+
+      await imageData.save();
+      res.status(201).json(imageData);
+    } else {
+      res.status(400).json({ error: 'No file uploaded' });
+    }
+  } catch (e) {
+    console.error('Error in image upload:', e);
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
