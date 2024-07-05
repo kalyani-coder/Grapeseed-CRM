b3db8b2255497f2c4d39b3b80cb8acc0009de2e4
Vedant0370 <vedant.0370@gmail.com>
2024-07-05 17:37:22 +0530

    update ExecutiveLogin.js

--- Backend/src/routes/ExecutiveLogin.js
+++ Backend/src/routes/ExecutiveLogin.js
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken');
 const jwtSecret = 'grapseeds';
 
 // Page: Get all clients
+
 router.get("/", async (req, res) => {
   try {
     const clients = await Client.find();
@@ -15,6 +16,7 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Get route for all value 
 router.get("/:field/:value", async (req, res) => {
   const field = req.params.field;
   const value = req.params.value.replace("_", " ");
@@ -94,7 +96,7 @@ router.post('/login', async (req, res) => {
   }
 });
 
-
+// Delete route 
 router.delete("/:id" , async(req, res) =>{
   try{
     const id = req.params.id
@@ -108,71 +110,4 @@ router.delete("/:id" , async(req, res) =>{
   }
 })
 
-
-// router.get("/login", async (req, res) => {
-//   const { ident, password } = req.body;
-//   const client = await Client.findOne({
-//     $or: [{ clientEmail: ident, clientPhone: ident }],
-//   });
-
-//   if (!client) {
-//     res
-//       .status(400)
-//       .json({ message: "Email or Phone not found", verified: false });
-//   }
-
-//   const isMatch = await client.comparePassword(password);
-
-//   if (!isMatch) {
-//     return res
-//       .status(400)
-//       .json({ message: "Invalid password", verified: false });
-//   }
-
-//   res.status(200).json({ message: "Valid password", verified: false });
-// });
-
-// Page: Create a new client
-// router.post("/", async (req, res) => {
-//   try {
-//     const newClient = new Client(req.body);
-//     await newClient.save();
-//     res.status(201).json(newClient);
-//   } catch (error) {
-//     res.status(500).json({ message: "Internal Server Error" });
-//   }
-// });
-
-// // Page: Update a client by ID
-// router.patch("/:id", async (req, res) => {
-//   const clientId = req.params.id;
-//   try {
-//     const updatedClient = await Client.findByIdAndUpdate(clientId, req.body, {
-//       new: true,
-//     });
-//     res.json(updatedClient);
-//   } catch (error) {
-//     res.status(404).json({ message: "Client not found" });
-//   }
-// });
-
-// // Page: Delete a client by ID
-// router.delete("/:id", async (req, res) => {
-//   const clientId = req.params.id;
-//   try {
-//     const deletedClient = await Client.findByIdAndDelete(clientId);
-
-//     if (!deletedClient) {
-//       return res.status(404).json({ message: "Client not found" });
-//     }
-
-//     res.json({ message: "Client deleted successfully", deletedClient });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: "Internal Server Error" });
-//   }
-// });
-
-
-// get by id
 module.exports = router;
